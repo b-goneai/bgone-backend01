1ac0923395e2ab7c25a773adabc56a370131cd42
b-goneai <bgoneai01@gmail.com>
2025-07-11 19:14:39 +0500

    Update modnet_utils.py

--- modnet_utils.py
+++ modnet_utils.py
@@ -1,21 +1,23 @@
+from modnet.model import MODNet  # update to match your model path
+import torch
 import numpy as np
 from PIL import Image
 
-def preprocess(image):
-    image = image.resize((512, 512))
-    image_np = np.array(image).astype(np.float32) / 255.0
-    image_np = image_np.transpose(2, 0, 1)
-    image_np = np.expand_dims(image_np, axis=0)
-    return image_np
+def load_modnet_model(ckpt_path):
+    model = MODNet(backbone_pretrained=False)
+    model.load_state_dict(torch.load(ckpt_path, map_location=torch.device('cpu')))
+    model.eval()
+    return model
 
-def postprocess(result, original_image):
-    mask = result[0][0]
-    mask = (mask * 255).astype(np.uint8)
-    mask = Image.fromarray(mask).resize(original_image.size)
-    rgba_image = original_image.convert("RGBA")
-    datas = rgba_image.getdata()
-    newData = []
-    for item, alpha in zip(datas, mask.getdata()):
-        newData.append((*item[:3], alpha))
-    rgba_image.putdata(newData)
-    return rgba_image
+def run_modnet(modnet, image: Image.Image) -> Image.Image:
+    img = image.resize((512, 512))
+    img_np = np.array(img).astype(np.float32) / 255.0
+    img_tensor = torch.from_numpy(img_np.transpose((2, 0, 1))).unsqueeze(0)
+
+    with torch.no_grad():
+        matte = modnet(img_tensor)[0][0].numpy()
+
+    matte = (matte * 255).astype(np.uint8)
+    alpha = Image.fromarray(matte).convert("L")
+    img.putalpha(alpha)
+    return img
